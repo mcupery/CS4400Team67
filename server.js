27e0ce2040a900714ee7044161bf08279b926b6c
Marcia Schulman <mcupery3@gatech.edu>
2018-04-07 12:02:39 -0400

    Added morgan, body-parser, compression, method-override modules as dependencies Added repository property to package.json  Changes to be committed: 	modified:   config/express.js 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'; //set dev as default environment.
+
 var express = require('./config/express');
 var app = express(); //calls express as s function that returns the app instance.
 
